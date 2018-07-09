1731afe6e61fa472b867a7d5cf659213627f3b91
Lightwing Ng <rodney_ng@icloud.com>
2018-07-09 17:59:36 +0800

    interface to database

---
+++ src/com/lightwing/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.lightwing.dao;
+
+import java.sql.SQLException;
+
+import com.lightwing.domain.UserBean;
+
+public interface UserDao {
+    /**
+     * 执行登录，并且返回该用户的所有信息
+     *
+     * @param user 执行登录的用户信息
+     * @return
+     */
+    UserBean login(UserBean user) throws SQLException;
+}
